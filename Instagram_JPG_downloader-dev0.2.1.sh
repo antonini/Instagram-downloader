594fcc00d7adff6ae0b315592550740ca9f6051b
andersbateva <andersbateva@gmail.com>
2016-07-05 20:41:17 -0300

    Versão 0.2.1 do script
    Adicionadas umas verificações extras para o script baixar só a versão de maior qualidade de cada foto.

--- Instagram_JPG_downloader-dev0.2.1.sh
+++ Instagram_JPG_downloader-dev0.2.1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Instagram JPG downloader		      # Baixador de JPG do Instagram
+# Instagram JPG downloader		# Baixador de JPG do Instagram
 # created on 2016 by Anders Bateva	# criado em 2016 por Anders Bateva
 
 #apresentação
 {
-	echo "Instagram JPG downloader - dev 0.2 (3 de julho de 2016)";
+	echo "Instagram JPG downloader - dev 0.2.1 (3 de julho de 2016)";
 	echo "================";
-	echo "Descrição: este script destina-se a baixar todas as fotos de um(a) determinado(a) usuário(a) do Instagram. Vídeos AINDA não são suportados direito (o script baixa só a miniatura JPG).";
+	echo "Descrição: este script destina-se a baixar todas as fotos de um(a) determinado(a) usuário(a) do Instagram, uma vez por foto (ou seja, o arquivo original, e não a miniatura). Vídeos AINDA não são suportados direito (o script baixa só a miniatura JPG).";
 	echo "----";
 	echo "Insira o nome de usuário(a) que consta na URL da página de perfil. Fica em \"https://www.instagram.com/<nome de usuário(a)>\". Será criado um diretório com este nome na localização atual, e dentro serão salvos os arquivos baixados.";
-	read -p "Usuário-alvo: " nome; #eu podia testar se o nome de usuário existe, em primeiro lugar, né?
+	read -p "Usuário(a)-alvo: " nome; #eu podia testar se o nome de usuário existe, em primeiro lugar, né?
 	echo "----";
 }
 
@@ -19,12 +19,16 @@ cd $nome; #entrando no diretório ... não dá pra criar e entrar de uma vez?
 #passo 1 - baixar todas os índices da conta (ciclo)
 {
 	#pontapé inicial para o index 1
-	{ 
+	{
 		contador_pagina=1; #contador do número da página atual, começa em 1
 		prox_pag=true; #só pra poder entrar no ciclo
+
 		wget -nd -q -O indice$contador_pagina.html https://www.instagram.com/$nome/; #baixando o index da conta, silenciosamente, sem sub-diretórios
 		echo -n "1) Baixado índice da página $contador_pagina...";
-		grep -o https://scontent.cdninstagram.com/[a-z0-9/\._-]*\.jpg indice$contador_pagina.html >> links.html; #extraindo links JPG e adicionando ao fim do arquivo de links - no futuro vai ter que pegar vídeo também!
+
+		grep -o '"profile_pic_url_hd": "https://scontent.cdninstagram.com/[a-z0-9/\._-]*\.jpg' indice1.html | grep -o "https://scontent.cdninstagram.com/[a-z0-9/\._-]*\.jpg" >> links.html; #extraindo foto de pefil
+		grep -o '"display_src": "https://scontent.cdninstagram.com/[a-z0-9/\._-]*\.jpg' indice$contador_pagina.html | grep -o "https://scontent.cdninstagram.com/[a-z0-9/\._-]*\.jpg" >> links.html; #extraindo links JPG e adicionando ao fim do arquivo de links - no futuro vai ter que pegar vídeo também!
+
 		total_links=$(wc -l < links.html);
 		echo " links extraídos - total: $total_links;";
 	}
@@ -42,7 +46,7 @@ cd $nome; #entrando no diretório ... não dá pra criar e entrar de uma vez?
 			echo -n "1.$contador_pagina) Detectado índice da página $contador_pagina...";
 			wget -nd -q -O indice$contador_pagina.html https://www.instagram.com/$nome/?max_id=$ult_img_pag; #baixando próximo índice
 			echo -n " baixado...";
-			grep -o https://scontent.cdninstagram.com/[a-z0-9/\._-]*\.jpg indice$contador_pagina.html >> links.html; #extraindo links JPG e adicionando ao fim do arquivo de links - no futuro vai ter que pegar vídeo também!
+			grep -o '"display_src": "https://scontent.cdninstagram.com/[a-z0-9/\._-]*\.jpg' indice$contador_pagina.html | grep -o "https://scontent.cdninstagram.com/[a-z0-9/\._-]*\.jpg" >> links.html; #extraindo links JPG e adicionando ao fim do arquivo de links - no futuro vai ter que pegar vídeo também!
 			total_links=$(wc -l < links.html);
 			echo " links extraídos - total: $total_links;";
 		fi
@@ -52,19 +56,20 @@ cd $nome; #entrando no diretório ... não dá pra criar e entrar de uma vez?
 }
 
 #passo 2 - baixar as fotos extraídas
-{ 
-	echo -n "2) Começando a baixar as imagens (JPGs) linkadas...";
+{
+	echo -n "2) Começando a baixar as $total_links imagens (JPGs) linkadas...";
 	wget -q -i links.html; #silenciosamente, sem sub-diretórios
 	echo " concluído!";
 }
+
 #despedida
 {
 	echo "Fim de execução.";
 	echo "----";
 	echo "A melhorar no futuro (to-do):";
-	echo "* o script baixa 3 vezes a foto de perfil, por página! - horrível;";
 	echo "* o script não consegue baixar os vídeos - não tá legal;";
-	echo "* o script baixa 2 vezes as mesmas imagens (miniatura e original) - ruim para contas grandes.";
-	echo "* o script podia detectar se o(a) usuário(a) tem a conta privada ou pública...";
+	echo "* o script poderia detectar se o(a) usuário(a) tem a conta privada ou pública...";
+	echo "* o script poderia detectar se o nome de usuário(a) realmente existe...";
 	echo "----";
 }
+#75 linhas
